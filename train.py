3f30bdf0867e628e78139b04ba5b40d2c4389c53
unajun <bjyoon513@gmail.com>
2021-05-05 14:48:00 +0900

    fix tiny issues and bug

--- train.py
+++ train.py
@@ -1,38 +1,38 @@
-import numpy as np
-import pandas as pd
+import timm
 import torch
-import torch.nn as nn
-import torch.utils.model_zoo as model_zoo
 from torch.utils.data import DataLoader
 
-from model import *
 from dataloader import LoadDataset
-import timm
+from model import *
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-data_cfgs = {"name": "DL20", "num_classes": 20, "dir":"./data/DL20"}
-train_cfgs = {"batch_size": 32, "lr": 0.0002, "total_epoch":20}
+data_cfgs = {"name": "DL20", "num_classes": 20, "dir": "DL20"}
+train_cfgs = {"batch_size": 32, "lr": 0.0002, "total_epoch": 20}
 
 ### load small version of ResNet
 # model = Small_ResNet(BasicBlock, [3, 3, 3], num_classes=data_cfgs['num_classes']).to('cuda')
 model = timm.create_model('tresnet_xl', pretrained=True, num_classes=data_cfgs["num_classes"])
+if torch.cuda.is_available():
+    model.cuda()
 
 ### load train/valid/test dataset
 train_dataset = LoadDataset(data_cfgs["dir"], mode="train", random_flip=True)
 valid_dataset = LoadDataset(data_cfgs["dir"], mode="valid", random_flip=False)
-#test_dataset = LoadDataset(data_cfgs["dir"], mode="test", random_flip=False)
+# test_dataset = LoadDataset(data_cfgs["dir"], mode="test", random_flip=False)
 
 ### warp dataset using dataloader
-train_dataloader = DataLoader(train_dataset, batch_size=train_cfgs["batch_size"], shuffle=True, pin_memory=True, drop_last=True)
-valid_dataloader = DataLoader(valid_dataset, batch_size=train_cfgs["batch_size"], shuffle=False, pin_memory=True, drop_last=False)
-#test_dataloader = DataLoader(test_dataset, batch_size=train_cfgs["batch_size"], shuffle=False, pin_memory=True, drop_last=False)
+train_dataloader = DataLoader(train_dataset, batch_size=train_cfgs["batch_size"], shuffle=True, pin_memory=True,
+                              drop_last=True)
+valid_dataloader = DataLoader(valid_dataset, batch_size=train_cfgs["batch_size"], shuffle=False, pin_memory=True,
+                              drop_last=False)
+# test_dataloader = DataLoader(test_dataset, batch_size=train_cfgs["batch_size"], shuffle=False, pin_memory=True, drop_last=False)
 
 ### define Adam optimizer: one of the popular optimizers in Deep Learning community
 optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), train_cfgs["lr"], eps=1e-6)
 
 ### define cross-entropy loss for classification
 criterion = nn.CrossEntropyLoss()
-
 
 ####################################################################################################
 ### Start training ###
@@ -54,7 +54,7 @@ while epoch <= train_cfgs["total_epoch"]:
         step += 1
         epoch += 1
 
-    images, labels = images.to('cuda'), labels.to('cuda')
+    images, labels = images.to(device), labels.to(device)
     logits = model(images)
     loss = criterion(logits, labels)
 
@@ -68,10 +68,10 @@ while epoch <= train_cfgs["total_epoch"]:
         model.eval()
         with torch.no_grad():
             for images, labels in iter(valid_dataloader):
-                images, labels = images.to('cuda'), labels.to('cuda')
+                images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-        print('\nAccuracy of the network on the {num_test} valid images: \
-              {acc}'.format(num_test=len(valid_dataset), acc=100*correct/total))
+        print(f'\nEpoch {epoch} Accuracy of the network on the {len(valid_dataset)} valid images: \
+              {100 * correct / total}')
